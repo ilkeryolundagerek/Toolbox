763610dd6453e944e2cef6923145fcbf0ce2aab1
ilker turan <ilkerturan@outlook.com>
2023-02-02 16:12:24 +0300

    02.02.2023 16:10

---
+++ Toolbox/Extensions/ForAutoMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Extensions
+{
+    public static class ForAutoMapper
+    {
+        public static IMappingExpression<TSource,TDestination> IgnoreAll<TSource, TDestination>(this IMappingExpression<TSource, TDestination> expression)
+        {
+            expression.ForAllMembers(o => o.Ignore());
+            return expression;
+        }
+    }
+}
