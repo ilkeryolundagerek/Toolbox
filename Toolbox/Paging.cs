71d9ba1f101a6c32fcf18832e6bf44e117dd3f08
BT06 <BT06@BT06>
2023-02-08 13:41:48 +0300

    08.02.2023 13:40

---
+++ Toolbox/Paging.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.DataTools;
+
+namespace Toolbox
+{
+    public class Paging<T> where T : class
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int Total { get; set; }
+        public List<T> PageData { get; set; }
+        public Paging(IEnumerable<T> data, int currentPage = 1, int pageSize = 20)
+        {
+            CurrentPage=currentPage;
+            PageSize=pageSize;
+            Total = data.Count();
+            TotalPages=Total%PageSize!=0 ? Total/pageSize+1 : Total/pageSize;
+            PageData = data.Skip((CurrentPage-1)*PageSize).Take(PageSize).ToList();
+        }
+    }
+}
